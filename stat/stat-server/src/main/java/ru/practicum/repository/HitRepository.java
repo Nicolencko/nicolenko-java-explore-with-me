83943da24d22dc5634ca0c752ab87be53810a34b
Nicolencko <111676776+Nicolencko@users.noreply.github.com>
2023-08-20 09:04:00 +0300

    Add files via upload

--- stat/stat-server/src/main/java/ru/practicum/repository/HitRepository.java
+++ stat/stat-server/src/main/java/ru/practicum/repository/HitRepository.java
@@ -2,6 +2,7 @@ package ru.practicum.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.practicum.ViewStats;
 import ru.practicum.model.Hit;
 
@@ -10,30 +11,15 @@ import java.util.List;
 
 public interface HitRepository extends JpaRepository<Hit, Long> {
 
-    @Query(value = "SELECT new ru.practicum.ViewStats(h.app, h.uri, count(h.uri)) " +
-            "FROM Hit as h " +
-            "WHERE h.timestamp >= ?1 AND h.timestamp <= ?2 " +
-            "GROUP BY h.app, h.uri")
-    List<ViewStats> getStatsOnlyDates(LocalDateTime start, LocalDateTime end);
+    @Query(value = "SELECT new ru.practicum.ViewStats(h.app, h.uri, count(h.uri)) " + "FROM Hit as h " + "WHERE h.timestamp >= :start AND h.timestamp <= :end " + "GROUP BY h.app, h.uri")
+    List<ViewStats> getStatsOnlyDates(@Param("start") LocalDateTime start, @Param("end") LocalDateTime end);
 
-    @Query(value = "SELECT new ru.practicum.ViewStats(h.app, h.uri, count(DISTINCT h.uri)) " +
-            "FROM Hit as h " +
-            "WHERE h.timestamp >= ?1 AND h.timestamp <= ?2 " +
-            "GROUP BY h.app, h.uri")
-    List<ViewStats> getStatsUniqueUris(LocalDateTime start, LocalDateTime end);
+    @Query(value = "SELECT new ru.practicum.ViewStats(h.app, h.uri, count(DISTINCT h.uri)) " + "FROM Hit as h " + "WHERE h.timestamp >= :start AND h.timestamp <= :end  " + "GROUP BY h.app, h.uri")
+    List<ViewStats> getStatsUniqueUris(@Param("start") LocalDateTime start, @Param("end") LocalDateTime end);
 
-    @Query(value = "SELECT new ru.practicum.ViewStats(h.app, h.uri, count(h.uri)) " +
-            "FROM Hit as h " +
-            "WHERE h.timestamp >= ?1 AND h.timestamp <= ?2 " +
-            "AND h.uri IN ?3 " +
-            "GROUP BY h.app, h.uri")
-    List<ViewStats> getStatsUrisList(LocalDateTime start, LocalDateTime end, List<String> uris);
+    @Query(value = "SELECT new ru.practicum.ViewStats(h.app, h.uri, count(h.uri)) " + "FROM Hit as h " + "WHERE h.timestamp >= :start AND h.timestamp <= :end " + "AND h.uri IN :uris " + "GROUP BY h.app, h.uri")
+    List<ViewStats> getStatsUrisList(@Param("start") LocalDateTime start, @Param("end") LocalDateTime end, @Param("uris") List<String> uris);
 
-    @Query(value = "SELECT new ru.practicum.ViewStats(h.app, h.uri, count(DISTINCT h.uri)) " +
-            "FROM Hit as h " +
-            "WHERE h.timestamp >= ?1 AND h.timestamp <= ?2 " +
-            "AND h.uri IN ?3" +
-            "GROUP BY h.app, h.uri")
-    List<ViewStats> getStatsUniqueUrisList(LocalDateTime start, LocalDateTime end, List<String> uris);
-
+    @Query(value = "SELECT new ru.practicum.ViewStats(h.app, h.uri, count(DISTINCT h.uri)) " + "FROM Hit as h " + "WHERE h.timestamp >= :start AND h.timestamp <= :end " + "AND h.uri IN :uris " + "GROUP BY h.app, h.uri")
+    List<ViewStats> getStatsUniqueUrisList(@Param("start") LocalDateTime start, @Param("end") LocalDateTime end, @Param("uris") List<String> uris);
 }
